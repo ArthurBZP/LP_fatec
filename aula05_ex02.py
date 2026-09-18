21b3a8e5b48aaed6051820e75df2655ea9aea869
ArthurBZP <arthurbiscaro1@gmail.com>
2026-09-18 11:56:41 -0300

    exercicio 2 e 3

---
+++ aula05_ex02.py
@@ -0,0 +1,12 @@
+def multiplo (a, b):
+    if a % b == 0:
+        return True
+    else:
+        return False
+    
+resultado = multiplo(8, 4)
+print(resultado)
+resultado = multiplo(7, 4)
+print(resultado)
+resultado = multiplo(5, 5)
+print(resultado)
---
+++ aula07_ex03.py
@@ -0,0 +1,7 @@
+def quadrado (lado):
+    return lado * lado
+
+resultado = quadrado(4)
+print(resultado)
+resultado = quadrado(9)
+print(resultado)
