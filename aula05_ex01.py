f2322bdd3341ca482e76c1c2804a207b13b6c95e
ArthurBZP <arthurbiscaro1@gmail.com>
2026-09-18 11:46:39 -0300

    exercicio 1

---
+++ aula05_ex01.py
@@ -0,0 +1,14 @@
+def maiorvalor (numero1, numero2):
+    if numero1 > numero2:
+        return numero1
+    else:
+        return numero2
+    
+resultado = maiorvalor (7, 2)
+print(resultado)
+resultado = maiorvalor (8, 9)
+print(resultado)
+resultado = maiorvalor (40, 5)
+
+        
+
